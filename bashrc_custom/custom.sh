1d9c11b725783fdc04cc16dfafe16f66755b9314
pedrocostacalazans <pedrocostacalazans@gmail.com>
2021-05-30 11:37:33 -0300

    2021.05.30 11:37

--- bashrc_custom/custom.sh
+++ bashrc_custom/custom.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cp ~/.bashrc ~/.bashrc.old
 cat trecho >> ~/.bashrc
+clear
+echo 'Pode-se ainda descomentar ou acrescentar:'
+echo
+echo "alias ll='ls -l'"
+echo "alias la='ls -A'"
+echo "alias l='ls -CF'"
+echo "alias cp='cp -i' <-- Este é para perguntar se quer subscrever..."
+echo
+
